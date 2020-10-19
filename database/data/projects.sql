336cbe18aa5f685a39abb953e975979fae1c0b74
aufarr16 <aufarr@gmail.com>
2020-10-19 17:48:00 +0700

    update db

--- database/data/projects.sql
+++ database/data/projects.sql
@@ -1,16 +1,15 @@
-INSERT INTO `projects` (`id_project`, `waktu_assign_project`, `id_pketerangan`, `id_pstat`, `id_ptype`, `id_product`, `id_user`, `ABA`, `nama_project`, `status_handover`, `pketerangan_note`) VALUES
-(1, '2020-08-25 21:46:34', NULL, 2, 2, 5, 2, '426', 'mari bekerja untuk masa depan yg lebih maju', 0, NULL),
-(2, '2020-08-25 21:46:34', NULL, 1, 1, 4, 4, '002', 'internal tes bersama bang hadi sans', 0, NULL),
-(3, '2020-08-25 21:46:34', 1, 3, 5, 3, 1, '011', 'QA Payment bankgakan orang tua mu nak', 0, NULL),
-(4, '2020-08-25 21:54:00', 2, 3, 3, 2, 5, '061', 'regresi bank bank tut syapa yg kentut', 1, NULL),
-(5, '2020-08-25 21:46:34', 2, 4, 2, 6, 6, '950', 'qris sakti mantab djiwa', 0, NULL),
-(6, '2020-08-26 04:58:48', 2, 4, 1, 1, 7, '517', 'terkadang, hidup memang seperti itu. iya. itu.', 1, NULL),
-(7, '2020-08-25 21:53:32', NULL, 5, 3, 3, 5, '016', 'hold it right there mister', 1, NULL),
-(8, '2020-08-25 21:53:32', 3, 3, 2, 6, 7, '069', 'projek apa hayoooo, cari tau sendiri lah', 0, 'ada yg kurang, kamu kurang perhatian'),
-(9, '2020-08-26 11:54:24', NULL, 1, 2, 3, 4, '115', 'projek pertama yg masuk lewat assign project, LETS GOO!!!', 0, NULL),
-(10, '2020-09-04 08:25:12', NULL, 2, 1, 6, 1, '360001', 'cico cico di dinding', 0, NULL),
-(11, '2020-09-04 08:25:12', NULL, 6, 5, 3, 2, '095', 'sistem pembayaran iuran biar pada gak telat', 0, NULL),
-(12, '2020-09-04 08:25:12', NULL, 2, 3, 5, 4, '503', 'disbursement adalah keahlian hadi, chakep', 1, NULL),
-(13, '2020-09-04 08:25:12', NULL, 2, 4, 1, 6, '114', 'life support by nadia', 0, NULL),
-(14, '2020-09-04 08:25:12', NULL, 2, 3, 4, 2, '808', 'regresi remittance bankkrut', 0, NULL);
-
+INSERT INTO `projects` (`id_project`, `waktu_assign_project`, `id_pketerangan`, `id_pstat`, `id_ptype`, `id_product`, `id_user`, `ABA`, `nama_project`, `status_handover`, `pketerangan_status`, `pketerangan_note`) VALUES
+(1, '2020-08-25 14:46:34', NULL, 2, 2, 5, 2, '426', 'mari bekerja untuk masa depan yg lebih maju', 0, NULL, NULL),
+(2, '2020-08-25 14:46:34', NULL, 1, 1, 4, 4, '002', 'internal tes bersama bang hadi sans', 0, NULL, NULL),
+(3, '2020-08-25 14:46:34', 1, 3, 5, 3, 1, '011', 'QA Payment bankgakan orang tua mu nak', 0, NULL, NULL),
+(4, '2020-10-19 08:06:53', 2, 3, 3, 2, 5, '061', 'regresi bank bank tut syapa yg kentut', 1, 'Menunggu Approval Pengujian Done', NULL),
+(5, '2020-10-19 08:07:27', 2, 4, 2, 6, 6, '950', 'qris sakti mantab djiwa', 0, 'Menunggu Approval Projek Done', NULL),
+(6, '2020-10-19 08:07:45', 2, 4, 1, 1, 7, '517', 'terkadang, hidup memang seperti itu. iya. itu.', 1, 'Menunggu Approval Projek Done', NULL),
+(7, '2020-08-25 14:53:32', NULL, 5, 3, 3, 5, '016', 'hold it right there mister', 1, NULL, NULL),
+(8, '2020-08-25 14:53:32', 3, 3, 2, 6, 7, '069', 'projek apa hayoooo, cari tau sendiri lah', 0, NULL, 'ada yg kurang, kamu kurang perhatian'),
+(9, '2020-08-26 04:54:24', NULL, 1, 2, 3, 4, '115', 'projek pertama yg masuk lewat assign project, LETS GOO!!!', 0, NULL, NULL),
+(10, '2020-09-04 01:25:12', NULL, 2, 1, 6, 1, '360001', 'cico cico di dinding', 0, NULL, NULL),
+(11, '2020-09-04 01:25:12', NULL, 6, 5, 3, 2, '095', 'sistem pembayaran iuran biar pada gak telat', 0, NULL, NULL),
+(12, '2020-09-04 01:25:12', NULL, 2, 3, 5, 4, '503', 'disbursement adalah keahlian hadi, chakep', 1, NULL, NULL),
+(13, '2020-09-04 01:25:12', NULL, 2, 4, 1, 6, '114', 'life support by nadia', 0, NULL, NULL),
+(14, '2020-09-04 01:25:12', NULL, 2, 3, 4, 2, '808', 'regresi remittance bankkrut', 0, NULL, NULL);
