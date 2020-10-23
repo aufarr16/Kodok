6942d94dd249a6f5f22f3929750063d22c02288a
aufarr16 <aufarr@gmail.com>
2020-10-23 17:38:28 +0700

    balikin form mitra

---
+++ resources/views/Layouts/FormMitra.blade.php
@@ -0,0 +1,30 @@
+{!! Form::model($model, [
+	'route' => $model->exist ? ['mitra.update', $model->id] : 'mitra.store',
+	'method' => $model->exist ? 'PUT' : 'POST'
+]) !!}
+	
+	<div class="form-group">
+		<div class ="input-group-addon">
+			<label for="ABA" style="font-weight:bolder" style="margin-top: -30px">ABA</label>
+				<br>
+			{!! Form::number('ABA', null,
+				['class'=>'form-control' .($errors->has('ABA') ? 'is-invalid' : ''), 
+					'id' =>'ABA',
+					'style' =>'margin-bottom: 10px'])
+			!!}		
+		</div>
+	</div>
+
+	<div class="form-group">
+		<div class ="input-group-addon">
+			<label for="nama_mitra" style="font-weight:bolder" style="margin-top: -30px">Nama Mitra</label>
+				<br>
+			{!! Form::text('nama_mitra', null,
+				['class'=>'form-control' .($errors->has('nama_mitra') ? 'is-invalid' : ''), 
+					'id' =>'nama_mitra', 
+					'style' =>'margin-bottom: 10px'])
+			!!}		
+		</div>
+	</div>
+
+{!! Form::close() !!}
