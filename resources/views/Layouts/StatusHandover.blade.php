3cc4e98e7413f9681cb91b059a4ed3e3924032d1
aufarr16 <aufarr@gmail.com>
2020-12-22 14:27:24 +0700

    ubah dikit

--- resources/views/Layouts/StatusHandover.blade.php
+++ resources/views/Layouts/StatusHandover.blade.php
@@ -2,13 +2,13 @@
 	@method('patch')
 	@csrf
 	<input type="hidden" value="{{ $project->id }}" name="id">
-		<div class="input-group">
-			<select class="custom-select" name="id_pstat">
-				<option value="" hidden>{{ $project->nama_pstat }}</option>
-				@foreach($pstat as $stat)
-					<option value="{{ $stat->id }}">{{ $stat->nama_pstat }}</option>
-				@endforeach  
-			</select>
+	<div class="input-group">
+		<select class="custom-select" name="id_pstat">
+			<option value="" hidden>{{ $project->nama_pstat }}</option>
+			@foreach($pstat as $stat)
+				<option value="{{ $stat->id }}">{{ $stat->nama_pstat }}</option>
+			@endforeach  
+		</select>
 												
 		<button class="btn-ok" type="submit">OK</button>
 	</div>
