16b11d0f5a1a76092b956c550db7794c45503c0f
aufarr16 <aufarr@gmail.com>
2021-02-09 11:26:54 +0700

    rapihin dikit

--- public/js/script.js
+++ public/js/script.js
@@ -588,9 +588,6 @@ function changeStatusProject(id){
 			});
 
 			$.ajax({
-				// url: '/engineer/projects/cancel',
-				// type: 'GET',
-
 				success: function(response){
 					// console.log("masuk pak");
 					$('#table1').DataTable().ajax.reload();
