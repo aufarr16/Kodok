76ed093ebf964a24ee267bd8da8f04caaff778c8
renaldybgs <renaldybagas13@gmail.com>
2021-02-10 15:01:42 +0700

    progress sit uat

---
+++ resources/views/Layouts/FormProgress.php
@@ -0,0 +1,13 @@
+	<div class="form-group">
+		<div class ="input-group-addon">
+			<label for="progress_sit" style="font-weight:bolder" style="margin-top: -30px">Progress SIT</label>
+				<br>
+		</div>
+	</div>
+
+	<div class="form-group">
+		<div class ="input-group-addon">
+			<label for="progress_uat" style="font-weight:bolder" style="margin-top: -30px">Progress UAT</label>
+				<br>
+		</div>
+	</div>
