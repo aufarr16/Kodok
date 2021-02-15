beb82a92337ddbbc23f75c132442eb7252dc47a5
renaldybgs <renaldybagas13@gmail.com>
2021-02-15 21:56:33 +0700

    update progress, pic

---
+++ resources/views/Layouts/FormPic1.blade.php
@@ -0,0 +1,62 @@
+<form method="PUT" action="{{ route('pic.update') }}">
+  <div class="form-group">
+    <label for="id_pic_product" style="font-weight:bolder">PIC Product</label>
+    <br>
+    <div class="@error('id_pic_product') is-invalid @enderror">
+      <select id="id_pic_pm" class="js-states form-control @error ('id_pic_product') is-invalid @enderror" data-placeholder="Pilih PIC Product" style="width:100%" name="id_pic_product">
+        <option value="" hidden>PIC Product</option>
+        @foreach($listproduct as $lpr)
+          @if (old('id_pic_pm') == $lpm->id)
+            <option value="{{ $lpr->id }}" selected>{{ $lpr->nama_user }}</option>
+          @else 
+            <option value="{{ $lpr->id }}">{{ $lpr->nama_user }}</option>
+          @endif
+        @endforeach
+      </select>
+       @error('id_pic_pm')
+        <div class="invalid-feedback flash" style="margin-top: 2px">
+          {{ $message }}</div>
+      @enderror
+    </div>
+  </div>
+  <div class="form-group">
+    <label for="id_pic_am" style="font-weight:bolder">PIC AM</label>
+    <br>
+    <div class="@error('id_pic_am') is-invalid @enderror">
+      <select id="id_pic_am" class="js-states form-control @error ('id_pic_am') is-invalid @enderror" data-placeholder="Pilih PIC AM" style="width:100%" name="id_pic_am">
+        <option value="" hidden>PIC AM</option>
+        @foreach($listam as $lam)
+          @if (old('id_pic_am') == $lam->id)
+            <option value="{{ $lam->id }}" selected>{{ $lam->nama_user }}</option>
+          @else 
+            <option value="{{ $lam->id }}">{{ $lam->nama_user }}</option>
+          @endif
+        @endforeach
+      </select>
+       @error('id_pic_am')
+        <div class="invalid-feedback flash" style="margin-top: 2px">
+          {{ $message }}</div>
+      @enderror
+    </div>
+  </div>
+  <div class="form-group">
+    <label for="id_pic_pm" style="font-weight:bolder">PIC PM</label>
+    <br>
+    <div class="@error('id_pic_pm') is-invalid @enderror">
+      <select id="id_pic_pm" class="js-states form-control @error ('id_pic_pm') is-invalid @enderror" data-placeholder="Pilih PIC PM" style="width:100%" name="id_pic_pm">
+        <option value="" hidden>PIC PM</option>
+        @foreach($listpm as $lpm)
+          @if (old('id_pic_pm') == $lpm->id)
+            <option value="{{ $lpm->id }}" selected>{{ $lpm->nama_user }}</option>
+          @else 
+            <option value="{{ $lpm->id }}">{{ $lpm->nama_user }}</option>
+          @endif
+        @endforeach
+      </select>
+       @error('id_pic_pm')
+        <div class="invalid-feedback flash" style="margin-top: 2px">
+          {{ $message }}</div>
+      @enderror
+    </div>
+  </div>
+</form>
--- resources/views/Layouts/FormProgress.php
+++ resources/views/Layouts/FormProgress.php
@@ -1,7 +1,19 @@
+{!! Form::model($model, [
+	'route' => $model->exists ? ['progress.change', $model->id] : 'progress.edit',
+	'method' => $model->exists ? 'PUT' : 'POST'
+]) !!}
+
 	<div class="form-group">
 		<div class ="input-group-addon">
 			<label for="progress_sit" style="font-weight:bolder" style="margin-top: -30px">Progress SIT</label>
 				<br>
+			{!! Form::text('progress_sit', null,
+				['class'=> 'form-control',
+					'id' =>'progress_sit', 
+					'style' =>'margin-bottom: 10px'
+				])
+			!!}		
+			<!-- {!!$errors->first('nama_product','<div class="invalid-feedback flash">$message</div>')!!} -->
 		</div>
 	</div>
 
@@ -9,5 +21,14 @@
 		<div class ="input-group-addon">
 			<label for="progress_uat" style="font-weight:bolder" style="margin-top: -30px">Progress UAT</label>
 				<br>
+			{!! Form::text('progress_sit', null,
+				['class'=> 'form-control',
+					'id' =>'progress_sit', 
+					'style' =>'margin-bottom: 10px'
+				])
+			!!}		
+			<!-- {!!$errors->first('nama_product','<div class="invalid-feedback flash">$message</div>')!!} -->
 		</div>
 	</div>
+
+{!! Form::close() !!}
