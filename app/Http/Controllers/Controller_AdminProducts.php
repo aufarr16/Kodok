c94937de89b54e16f14077ae10685cafe2c138f8
aufarr16 <aufarr@gmail.com>
2021-02-17 14:29:41 +0700

    nambah comment di controller admin product

--- app/Http/Controllers/Controller_AdminProducts.php
+++ app/Http/Controllers/Controller_AdminProducts.php
@@ -30,12 +30,12 @@ class Controller_AdminProducts extends Controller
 	 */
 
     public function create() {
-        $model = new Product();
+        $model = new Product();             //membuat variable product baru untuk dilempar dan diisi di form Tambah Product
         return view('Layouts.FormProducts', compact('model'));
     }
 
     public function store(Request $request){
-        $request->validate([
+        $request->validate([                //validasi input pada form Tambah Product
             'nama_product' => 'required|max:24|regex:/^[a-zA-Z ]*$/',
         ],
         $message = [
@@ -44,8 +44,8 @@ class Controller_AdminProducts extends Controller
                 'nama_product.regex' => ' Nama Products hanya boleh berisi huruf',
         ]);
 
-        $added_by = Auth::user()->inisial_user;
-    	Product::create([
+        $added_by = Auth::user()->inisial_user;             //mengambil inisial untuk nanti dimasukkan ke kolom added_by pada tabel Product
+    	Product::create([                                   //menambah data product baru
             'nama_product' => $request->nama_product,
             'added_by' => $added_by
         ]);
@@ -54,20 +54,20 @@ class Controller_AdminProducts extends Controller
     }
 
     public function destroy($id){
-        Product::where('id', $id)->delete();
-        $productData['data'] = Product::orderby("id", "asc")->get();
+        Product::where('id', $id)->delete();                //mencari data product dan menghapusnya
+        $productData['data'] = Product::orderby("id", "asc")->get();    //mengambil semua data product setelah menghapus data
 
-        return response()->json($productData);
+        return response()->json($productData);              //melempar data product 
     }
 
     public function edit($id)
     {
-      $model = Product::where('id', $id)->firstOrFail();
+      $model = Product::where('id', $id)->firstOrFail();    //mengambil data yg akan diedit untuk ditempel di form Edit Product
       return view('Layouts.FormProducts', compact('model'));
     }
 
     public function update(Request $request, $id){
-        $request->validate([
+        $request->validate([                                //validasi input pada form Edit Product
             'nama_product' => 'required|max:24|regex:/^[a-zA-Z ]*$/',
         ],
         $message = [
@@ -76,17 +76,17 @@ class Controller_AdminProducts extends Controller
                 'nama_product.regex' => ' Nama Products hanya boleh berisi huruf',
         ]);
 
-        $modified_by = Auth::user()->inisial_user;
-        $model = Product::where('id', $id)->firstOrFail();
-        $model->nama_product = $request->nama_product;
-        $model->modified_by = $modified_by;
-        $model->save();
+        $modified_by = Auth::user()->inisial_user;          //mengabil inisial user untuk diisi di kolom modified_by pada tabel Product
+        $model = Product::where('id', $id)->firstOrFail();  //mengambil data yg akan diedit
+        $model->nama_product = $request->nama_product;      //edit nama product
+        $model->modified_by = $modified_by;                 //edit modified_by
+        $model->save();                                     //menyimpan perubahan
     }
 
     public function dataTable()
     {
-        $model = Product::query();
-        return DataTables::of($model)
+        $model = Product::query();                          //mengambil semua data product
+        return DataTables::of($model)                       //membuat datatable
             ->addColumn('action', function($model){
                 return view('Layouts.ActionProduct',[
                     'model'=> $model,
