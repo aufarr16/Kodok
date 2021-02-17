71a791857a1762591a53cdb1dc24bdce167f10ae
aufarr16 <aufarr@gmail.com>
2021-02-17 16:14:31 +0700

    ngapus dikit

--- app/Http/Controllers/Controller_EngineerYourProjects.php
+++ app/Http/Controllers/Controller_EngineerYourProjects.php
@@ -95,9 +95,6 @@ class Controller_EngineerYourProjects extends Controller
 
     public function editBussinessPIC($id){
         $project = $this->getProjectById($id);          //ngambil data projek yg dipilih
-        // $currentproduct = $project->id_pic_product;     //ngambil pic product skrng
-        // $currentam = $project->id_pic_am;               //ngambil pic am skrng
-        // $currentpm = $project->id_pic_pm;               //ngambil pic pm skrng
 
         //ngambil data buat ngisi dropdown
         $listproduct = $this->getProductData($id);
