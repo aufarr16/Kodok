c226854fed84ed210827a1b66b7329812b582797
renaldybgs <renaldybagas13@gmail.com>
2021-02-19 11:50:38 +0700

    list project detail

--- resources/views/Layouts/DetailProject.blade.php
+++ resources/views/Layouts/DetailProject.blade.php
@@ -1,41 +1,46 @@
-<div class="row">
- <div class="col-lg-12">
-  <table class="table table-bordered table-hover">
-   <thead>
-    <tr>
-     <th>PIC Saat ini</th>
-     <td>{{ $piccurrent->nama_user }}</td>
-    </tr>
-    <tr>
-    <tr>
-     <th>PIC Original</th>
-     <td>{{ $picori->nama_user }}</td>
-    </tr>
-    <tr>
-     <th>Nama PIC Product</th>
-     <td>{{ $picproduct->nama_user }}</td>
-    </tr>
-    <tr>
-     <th>Nama PIC PM</th>
-     <td>{{ $picpm->nama_user }}</td>
-    </tr>
-    <tr>
-     <th>Nama PIC AM</th>
-     <td>{{ $picam->nama_user }}</td>
-    </tr>
-    <tr>
-     <th>Nama PIC Bisnis</th>
-     <td>{{ $picbisnis->nama_user }}</td>
-    </tr>
-    <tr>
-     <th>Progress SIT</th>
-     <td>{{ $progress->progress_sit }}</td>
-    </tr>
-    <tr>
-     <th>Progress UAT</th>
-     <td>{{ $progress->progress_uat }}</td>
-    </tr>
-   </thead>
-  </table>
- </div>
-</div>
+{!! Form::model($listprojects, [
+    'route' => ['listprojects.detail', $listprojects->id],
+    'method' => 'GET'
+]) !!}
+    <div class="row">
+     <div class="col-lg-12">
+      <table class="table table-bordered table-hover">
+       <thead>
+        <tr>
+         <th>PIC Saat ini</th>
+         <td>{{ $piccurrent->nama_user }}</td>
+        </tr>
+        <tr>
+        <tr>
+         <th>PIC Original</th>
+         <td>{{ $picori->nama_user }}</td>
+        </tr>
+        <tr>
+         <th>Nama PIC Product</th>
+         <td>{{ $picproduct->nama_user }}</td>
+        </tr>
+        <tr>
+         <th>Nama PIC PM</th>
+         <td>{{ $picpm->nama_user }}</td>
+        </tr>
+        <tr>
+         <th>Nama PIC AM</th>
+         <td>{{ $picam->nama_user }}</td>
+        </tr>
+        <tr>
+         <th>Nama PIC Bisnis</th>
+         <td>{{ $picbisnis->nama_user }}</td>
+        </tr>
+        <tr>
+         <th>Progress SIT</th>
+         <td>{{ $progress->progress_sit }}</td>
+        </tr>
+        <tr>
+         <th>Progress UAT</th>
+         <td>{{ $progress->progress_uat }}</td>
+        </tr>
+       </thead>
+      </table>
+     </div>
+    </div>
+{!! Form::close() !!}
