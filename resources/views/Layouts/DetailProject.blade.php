474ba350f2bc173354fadb6376d2b12ffce5019d
renaldybgs <renaldybagas13@gmail.com>
2021-02-19 11:32:57 +0700

    detail project

--- resources/views/Layouts/DetailProject.blade.php
+++ resources/views/Layouts/DetailProject.blade.php
@@ -3,59 +3,37 @@
   <table class="table table-bordered table-hover">
    <thead>
     <tr>
-     <th>No</th>
-     <td>{{ $loop->iteration }}</td>
+     <th>PIC Saat ini</th>
+     <td>{{ $piccurrent->nama_user }}</td>
     </tr>
     <tr>
-     <th>Nama User</th>
-     <td>{{ $project->nama_user }}</td>
     <tr>
-    <tr>
-     <th>Inisial</th>
-     <td>{{ $project->inisial_user }}</td>
-    <tr>
-    <tr>
-     <th>Produk</th>
-     <td>{{ $project->nama_product }}</td>
-    <tr>
-     <th>Jenis Project</th>
-     <td>{{ $project->nama_ptype }}</td>
+     <th>PIC Original</th>
+     <td>{{ $picori->nama_user }}</td>
     </tr>
     <tr>
-     <th>Nama Mitra</th>
-     <td>{{ $project->nama_mitra }}</td>
-    </tr>
-    <tr>
-     <th>Nama Project</th>
-     <td>{{ $project->nama_project }}</td>
+     <th>History Pic</th>
+     <td>{{ $historypic->nama_user }}</td>
     </tr>
     <tr>
      <th>Nama PIC Product</th>
-     <td>{{ $project->nama_user }}</td>
+     <td>{{ $picbisnis->nama_user }}</td>
     </tr>
     <tr>
      <th>Nama PIC PM</th>
-     <td>{{ $project->nama_user }}</td>
+     <td>{{ $picbisnis->nama_user }}</td>
     </tr>
     <tr>
      <th>Nama PIC AM</th>
-     <td>{{ $project->nama_user }}</td>
+     <td>{{ $picbisnis->nama_user }}</td>
     </tr>
     <tr>
      <th>Progress SIT</th>
-     <td>{{ $project->progress_sit }}</td>
+     <td>{{ $progress->progress_sit }}</td>
     </tr>
     <tr>
      <th>Progress UAT</th>
-     <td>{{ $project->progress_uat }}</td>
-    </tr>
-    <tr>
-     <th>Tanggal Assign</th>
-     <td>{{ $project->tanggal_assign}}</td>
-    </tr>
-    <tr>
-     <th style="width: 75px">Status</th>
-     <td>{{ $project->id_pstat}}</td>
+     <td>{{ $progress->progress_uat }}</td>
     </tr>
    </thead>
   </table>
