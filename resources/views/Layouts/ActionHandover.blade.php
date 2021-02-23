77ece1e8d491a60ea9ddf313f47bee02d6e8a226
ismidstwt <ismidestiawati07@gmail.com>
2021-02-23 18:35:22 +0700

    benerin button PIC dan Progress di projects

--- resources/views/Layouts/ActionHandover.blade.php
+++ resources/views/Layouts/ActionHandover.blade.php
@@ -1,3 +1,3 @@
 <button onclick="donehandover(id)" type="submit" class="btn-handover-done" data-dismiss="modal" id="{{ $project->id }}" title="Handover selesai"><i class="fas fa-check-circle fa-lg"></i></button>
-<a href="{{ $url_pic }}" type="button" class="modal-show btn-edit" title="Ubah PIC" id="btn-modal"><i class="fas fa-plus fa-lg"></i></a>
-<a href="{{ $url_progress }}" type="button" class="modal-show btn-edit btn-progress" title="Ubah Progress" id="btn-modal"><i class="fas fa-sync-alt"></i></a>
+<a href="{{ $url_pic }}" type="button" class="modal-show btn-edit" title="Update PIC" id="btn-modal" style="padding: 6px 5px"><i class="fas fa-users fa-lg"></i></a>
+<a href="{{ $url_progress }}" type="button" class="modal-show btn-edit btn-progress" title="Update Progress" id="btn-modal" style="padding: 6px 8px"><i class="fas fa-percent fa-lg"></i></a>
