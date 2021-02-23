0c02bf1e3a8f60e1273dbed55c875ff465843586
ismidstwt <ismidestiawati07@gmail.com>
2021-02-23 18:48:10 +0700

    benerin ukuran button tambah PIC dan progress di projects

--- resources/views/Layouts/ActionProject.blade.php
+++ resources/views/Layouts/ActionProject.blade.php
@@ -1,2 +1,2 @@
-<a href="{{ $url_pic }}" type="button" class="modal-show btn-edit" title="Ubah PIC" id="btn-modal" style="padding: 6px 5px"><i class="fas fa-users fa-lg"></i></a>
-<a href="{{ $url_progress }}" type="button" class="modal-show btn-edit" title="Ubah Progress" id="btn-modal" style="padding: 6px 8px"><i class="fas fa-percent fa-lg"></i></a>
+<a href="{{ $url_pic }}" type="button" class="modal-show btn-edit" title="Ubah PIC" id="btn-modal" style="padding: 6px 3px; border-radius: 5px"><i class="fas fa-users fa-lg"></i></a>
+<a href="{{ $url_progress }}" type="button" class="modal-show btn-edit" title="Ubah Progress" id="btn-modal" style="padding: 6px 7px; border-radius: 5px"><i class="fas fa-percent fa-ld"></i></a>
