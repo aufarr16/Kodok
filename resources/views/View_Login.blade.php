0b03f293d67dcdf6d44a90f8c0bda6367e618755
renaldybgs <renaldybagas13@gmail.com>
2021-06-25 12:52:04 +0700

    login update css

--- resources/views/View_Login.blade.php
+++ resources/views/View_Login.blade.php
@@ -3,24 +3,33 @@
 
 <head>
   <meta charset="utf-8" />
-  <link rel="icon" type="image/png" href="{{ url('') }}/img/frog-solid.svg">
+  <link rel="icon" type="image/png" href="{{ url('assets/img/frog-solid.svg') }}">
   <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
   <title>
     | Login
   </title>
+ <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
+  <!-- Font Awesome -->
+  <link rel="stylesheet" href="{{ url('assets/plugins/fontawesome-free/css/all.min.css') }}">
+  <!-- icheck bootstrap -->
+  <link rel="stylesheet" href="{{ url('assets/plugins/icheck-bootstrap/icheck-bootstrap.min.css') }}">
+  <!-- Theme style -->
+  <link rel="stylesheet" href="{{ url('assets/dist/css/adminlte.min.css') }}">
+
+
   <meta content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0, shrink-to-fit=no' name='viewport' />
   <!--     Fonts and icons     -->
-  <link href="https://fonts.googleapis.com/css?family=Montserrat:400,700,200" rel="stylesheet" />
+<!--   <link href="https://fonts.googleapis.com/css?family=Montserrat:400,700,200" rel="stylesheet" />
   <link href="https://fonts.google.com/css?family=Balsamiq+Sans" rel="stylesheet" type="text/css"/>
-  <link href="https://maxcdn.bootstrapcdn.com/font-awesome/latest/css/font-awesome.min.css" rel="stylesheet"/>
+  <link href="https://maxcdn.bootstrapcdn.com/font-awesome/latest/css/font-awesome.min.css" rel="stylesheet"/> -->
   <!-- CSS Files -->
-  <link href="{{ url('') }}/css/bootstrap.min.css" rel="stylesheet" />
-  <link href="{{ url('') }}/css/paper-dashboard.css?v=2.0.0" rel="stylesheet" >
-  <link href="{{ url('') }}/css/Login.css" rel="stylesheet" >
+  <link href="{{ url('assets/bootstrap.min.css') }}" rel="stylesheet" />
+  <link href="{{ url('assets/paper-dashboard.css?v=2.0.0') }}" rel="stylesheet" >
+  <link href="{{ url('assets/Login.css') }}" rel="stylesheet" >
 
   <style>
     body {
-      background-image: url('{{ url('') }}/img/Abstract blue.jpg');
+      background-image: url('{{ url('assets/img/Abstract blue.jpg') }}');
       background-size: 100% 100%;}
   </style>
 </head>
@@ -77,4 +86,3 @@
 </div>
 
 <!-- Javascript -->
-<script src="https://kit.fontawesome.com/a076d05399.js"></script>
