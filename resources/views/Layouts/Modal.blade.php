87de5ed6b09dbf8b295983970ca5e331aea67ea2
renaldybgs <renaldybagas13@gmail.com>
2021-08-03 14:31:18 +0700

    update form modal title

--- resources/views/Layouts/Modal.blade.php
+++ resources/views/Layouts/Modal.blade.php
@@ -4,7 +4,7 @@
         <!-- Modal content -->
         <div class="modal-content">
             <div class="modal-header">
-                <h5 class="modal-title" id="exampleModalLabel">Form Input</h5>
+                <h5 class="modal-title" id="modal-title">Form Input</h5>
                 <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                     <span aria-hidden="true">&times;</span>
                 </button>
