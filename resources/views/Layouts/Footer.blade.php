61a9ab3f7375bf2a05e6b897cfb36d11668130e1
renaldybgs <renaldybagas13@gmail.com>
2021-09-21 15:40:39 +0700

    update

--- resources/views/Layouts/Footer.blade.php
+++ resources/views/Layouts/Footer.blade.php
@@ -1,9 +1,8 @@
-<!-- footer -->
-	<!-- <div class="blockquote text-left"> -->
+
 <footer class="main-footer">
     <strong>Copyright &copy; 2021 <a>KODOK</a>.</strong>
     All rights reserved.
     <div class="float-right d-none d-sm-inline-block">
-        <b>Version</b> 1.1.3
+        <b>Version</b> 1.1.4
     </div>
 </footer>
