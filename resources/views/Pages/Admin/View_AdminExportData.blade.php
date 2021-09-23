d50c9ed4c237f140e2c2501041251b2239796e0a
renaldybgs <renaldybagas13@gmail.com>
2021-09-23 15:09:41 +0700

    update  export

--- resources/views/Pages/Admin/View_AdminExportData.blade.php
+++ resources/views/Pages/Admin/View_AdminExportData.blade.php
@@ -7,10 +7,10 @@
 <div class="row">
   <div class="col-md-4 col-sm-6 col-12">
     <div class="info-box ">
-      <a href="#" class="btn btn-info info-box-icon"><i class="fas fa-download"></i></a>
+      <a href="/admin/export/user" class="btn btn-info info-box-icon"><i class="fas fa-download"></i></a>
 
       <div class="info-box-content">
-        <span class="info-box-text"><font size="4"><strong>Export All Data</strong></font></span>
+        <span class="info-box-text"><font size="4"><strong>Export User</strong></font></span>
         
       </div>
       <!-- /.info-box-content -->
@@ -20,7 +20,7 @@
   <!-- /.col -->
   <div class="col-md-4 col-sm-6 col-12">
     <div class="info-box">
-      <a href="#" class="btn btn-success info-box-icon"><i class="fas fa-download"></i></a>
+      <a href="/admin/export/mitra" class="btn btn-success info-box-icon"><i class="fas fa-download"></i></a>
 
       <div class="info-box-content">
         <span class="info-box-text"><font size="4"><strong>Export Mitra</strong></font></span>
@@ -32,7 +32,7 @@
   <!-- /.col -->
   <div class="col-md-4 col-sm-6 col-12">
     <div class="info-box">
-      <a href="#" class="btn btn-warning info-box-icon"><i class="fas fa-download"></i></a>
+      <a href="/admin/export/product" class="btn btn-warning info-box-icon"><i class="fas fa-download"></i></a>
 
       <div class="info-box-content">
         <span class="info-box-text"><font size="4"><strong>Export Product</strong></font></span>
@@ -47,17 +47,7 @@
 <div class="row">
     <div class="col-md-4 col-sm-6 col-12">
         <div class="info-box">
-            <a href="#" class="btn btn-danger info-box-icon"><i class="fas fa-download"></i></a>
-
-          <div class="info-box-content">
-            <span class="info-box-text"><font size="4"><strong>Export User</strong></font></span>
-          </div>
-        <!-- /.info-box-content -->
-        </div>
-    </div>
-    <div class="col-md-4 col-sm-6 col-12">
-        <div class="info-box">
-            <a href="/admin/export/project" class="btn bg-purple info-box-icon"><i class="fas fa-download"></i></a>
+            <a href="/admin/export/project" class="btn btn-danger info-box-icon"><i class="fas fa-download"></i></a>
 
           <div class="info-box-content">
             <span class="info-box-text"><font size="4"><strong>Export Project</strong></font></span>
@@ -67,10 +57,20 @@
     </div>
     <div class="col-md-4 col-sm-6 col-12">
         <div class="info-box">
-            <a href="#" class="btn bg-pink info-box-icon"><i class="fas fa-download"></i></a>
+            <a href="/admin/export/handover" class="btn bg-purple info-box-icon"><i class="fas fa-download"></i></a>
 
           <div class="info-box-content">
-            <span class="info-box-text"><font size="4"><strong>Export Jenis Project</strong></font></span>
+            <span class="info-box-text"><font size="4"><strong>Export Project Handover</strong></font></span>
+          </div>
+        <!-- /.info-box-content -->
+        </div>
+    </div>
+    <div class="col-md-4 col-sm-6 col-12">
+        <div class="info-box">
+            <a href="/admin/export/pketerangan" class="btn bg-pink info-box-icon"><i class="fas fa-download"></i></a>
+
+          <div class="info-box-content">
+            <span class="info-box-text"><font size="4"><strong>Export Project Keterangan</strong></font></span>
           </div>
         <!-- /.info-box-content -->
         </div>
@@ -78,6 +78,39 @@
     
 </div>
 
+<div class="row">
+    <div class="col-md-4 col-sm-6 col-12">
+        <div class="info-box">
+            <a href="/admin/export/pstat" class="btn bg-gray info-box-icon"><i class="fas fa-download"></i></a>
+
+          <div class="info-box-content">
+            <span class="info-box-text"><font size="4"><strong>Export Project Status</strong></font></span>
+          </div>
+        <!-- /.info-box-content -->
+        </div>
+    </div>
+    <div class="col-md-4 col-sm-6 col-12">
+        <div class="info-box">
+            <a href="/admin/export/ptype" class="btn bg-navy info-box-icon"><i class="fas fa-download"></i></a>
+
+          <div class="info-box-content">
+            <span class="info-box-text"><font size="4"><strong>Export Type Project</strong></font></span>
+          </div>
+        <!-- /.info-box-content -->
+        </div>
+    </div>
+    <div class="col-md-4 col-sm-6 col-12">
+        <div class="info-box">
+            <a href="/admin/export/ulevel" class="btn bg-orange info-box-icon"><i class="fas fa-download"></i></a>
+
+          <div class="info-box-content">
+            <span class="info-box-text"><font size="4"><strong>Export User Levels</strong></font></span>
+          </div>
+        <!-- /.info-box-content -->
+        </div>
+    </div>
+    
+</div>
 @endsection
 
 @push('scripts')
