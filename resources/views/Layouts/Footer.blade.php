e286e298aa2a2a6db1dfb94f124b3914305836f6
aufarr16 <aufarr@gmail.com>
2021-10-07 10:29:21 +0700

    rapihin footer

--- resources/views/Layouts/Footer.blade.php
+++ resources/views/Layouts/Footer.blade.php
@@ -3,6 +3,6 @@
     <strong>Copyright &copy; 2021 <a>KODOK</a>.</strong>
     All rights reserved. 
     <div class="float-right d-none d-sm-inline-block">
-        <b>Customer Service: +62 813-8500-0966 (Ismi)</b> <b>Version</b> 1.1.4
+        <b>Customer Service: +62 813-8500-0966 (Ismi) | </b> <b>Version</b> 1.1.4
     </div>
 </footer>
