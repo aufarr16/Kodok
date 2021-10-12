987d586298a9bcca452df7a6adaf1370afe28256
renaldybgs <renaldybagas13@gmail.com>
2021-10-12 16:55:05 +0700

    update footer link WA

--- resources/views/Layouts/Footer.blade.php
+++ resources/views/Layouts/Footer.blade.php
@@ -3,6 +3,6 @@
     <strong>Copyright &copy; 2021 <a>KODOK</a>.</strong>
     All rights reserved. 
     <div class="float-right d-none d-sm-inline-block">
-        <b>Customer Service: +62 813-8500-0966 (Ismi) | </b> <b>Version</b> 1.1.4
+        <strong>Customer Service </strong> <i class="fab fa-whatsapp"></i> <a href="https://api.whatsapp.com/send/?phone=6281385000966"><strong>Ismi Destiawati</strong></a> <b>| Version</b> 1.1.4
     </div>
 </footer>
