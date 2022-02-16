d365ae205b50be88921e434761c6de4f5f9c5a65
aufarr16 <aufarr@gmail.com>
2022-02-16 15:53:17 +0700

    diset null nodi dkk

--- app/Project.php
+++ app/Project.php
@@ -13,6 +13,11 @@ class Project extends Model
     	'handover_counter' => 0,
     	'progress_sit' => 0,
     	'progress_uat' => 0,
+    	'nodin_in' => '',
+    	'nodin_out' => '',
+    	'no_bako' => '',
+    	'no_bato' => '',
+    	'no_bae' => '',
     ];
 
     protected $fillable = ['id_product', 'id_ptype', 'id_mitra', 'nama_project', 'id_current_pic', 'id_original_pic', 'id_pic_product', 'id_pic_am', 'id_pic_pm', 'direktori_project', 'nodin_in', 'nodin_out', 'no_bako', 'no_bato', 'no_bae'];
