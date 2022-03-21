7bfa51affbb322199a23cd6d5ef0a4d2c78ee9e1
renaldybgs <renaldybagas13@gmail.com>
2022-03-21 15:23:49 +0700

    dah dah dah dah dah dah

--- resources/views/Layouts/KeteranganProject.blade.php
+++ resources/views/Layouts/KeteranganProject.blade.php
@@ -1,2 +1,1 @@
-{{ $project->pketerangan_status }} 
-<!-- <button type="button" class="btn-keterangan" title="Keterangan Status" data-toggle="modal" data-target="#modal1"><i class="far fa-question-circle"></i></button> -->    
+<a href="{{ $url_notes }}" type="button" class="modallist-show" title="{{ $project->pketerangan_status }}" data-target="#modallist" data-toggle="tooltip" data-placement="top">{{ $project->pketerangan_status }}</a>
